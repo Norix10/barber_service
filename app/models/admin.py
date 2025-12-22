9976d0772b5954e291a795731b0d8012ba118e9d
Norix <121669280+Norix10@users.noreply.github.com>
2025-12-22 12:12:29 +0200

    update base.py (not longer user UUID)

--- app/models/admin.py
+++ app/models/admin.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
 
+
 class Admin(Base):
     name: Mapped[str] = mapped_column(String(30), nullable=False)
     email: Mapped[str] = mapped_column(String(30), nullable=False)
