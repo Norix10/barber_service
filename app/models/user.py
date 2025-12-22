25c8bd9d728e3748841ac36a4dd3b5667ca4e6f4
Norix <121669280+Norix10@users.noreply.github.com>
2025-12-22 12:11:57 +0200

    create user model base

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models.base import Base
+
+
+class User(Base):
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(String(30), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
