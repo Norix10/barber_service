7a3724723da51797e4f304e5da6c2a77ebf54a4f
Norix <121669280+Norix10@users.noreply.github.com>
2025-12-22 12:11:02 +0200

    update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,21 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
 from pathlib import Path
 
-BASEDIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_PATH = BASE_DIR / ".env"
 
-class Settings():
-    url: str = 'postgresql+asyncpg://neondb_owner:npg_qruiahe0dnL4@ep-steep-dew-agw1y9c7-pooler.c-2.eu-central-1.aws.neon.tech/neondb'
-    echo: bool = False
-    
+class Settings(BaseSettings):
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRES_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRES_MINUTES: int = 15 * 24 * 60
+
+    SECRET_KEY: str
+    DATABASE_URL: str 
+    ECHO: bool = False
+
+    class Config:
+        env_file = str(ENV_PATH)
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+
 settings = Settings()
